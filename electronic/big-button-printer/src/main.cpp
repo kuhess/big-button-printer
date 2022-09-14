23ce971c47dc983f29ac4bb7abc13681a198d3e4
Quentin Suire <dev@quentinsuire.com>
2022-09-14 16:33:53 +0200

    print one row

--- electronic/big-button-printer/src/main.cpp
+++ electronic/big-button-printer/src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-#include <TPrinter.h>
+#include <ArduinoLog.h>
+#include <LittleFS.h>
 
-#include "img.h"
+#include "TPrinter.h"
 
 
 const byte dtrPin = D1;  // GPIO 5
@@ -12,26 +13,35 @@ const byte txPin = D10;  // GPIO 1
 const int printerBaudrate = 9600;
 // RX, TX are the same as hardware Serial
 // I declare a custom SoftwareSerial to not get ESP8266 Watchdog Timer logs
+// /!\ DO NOT USE SERIAL TO LOG, ONLY FOR PRINTER
 SoftwareSerial mySerial(rxPin, txPin);
 Tprinter printer(&mySerial, printerBaudrate);
 
 void setupPrinter() {
-  mySerial.begin(printerBaudrate);
-  printer.begin();
-  printer.enableDtr(dtrPin, HIGH);
-  printer.setCodePage(CODEPAGE_CP437);
-  printer.setCharset(CHARSET_USA);
+    mySerial.begin(printerBaudrate);
+    printer.begin();
+    printer.enableDtr(dtrPin, HIGH);
+    printer.setCodePage(CODEPAGE_CP437);
+    printer.setCharset(CHARSET_USA);
 }
 
 void setup() {
-  Serial.begin(115200);
+    setupPrinter();
 
-  setupPrinter();
+    LittleFS.begin();
 
-  printer.printBitmap(rocky, imgWidth, imgHeight, 1);
-  printer.feed(1);
+    // byte buffer[1152];
+    // for (size_t i = 0; i < 1152; i++) {
+    //     buffer[i] = 0b01010101;
+    // }
+    // printer.printRawImage(buffer, 384, 24);
+
+    File image_file = LittleFS.open("/line.raw", "r");
+    byte buffer[1152];
+    image_file.readBytes((char *) buffer, 1152);
+    printer.printRawImage(buffer, 384, 24);
 }
 
 void loop() {
-  delay(30000);
+    delay(100);
 }
---
+++ server/big-button-printer-server/script/generate_raw.py
@@ -0,0 +1,65 @@
+import click
+import more_itertools as mit
+from PIL import Image, ImageOps
+from PIL.Image import FLOYDSTEINBERG
+
+def ceiling_division(n, d):
+    return -(n // -d)
+
+def get_slices(image: Image) -> Image:
+    slice_height = 24
+    width, height = image.size
+
+    num_slices = ceiling_division(height, slice_height)
+
+    for i in range(num_slices):
+        box = (0, i*slice_height, width, (i+1)*slice_height)
+        slice = image.crop(box)
+        yield slice
+
+def slice_to_bytes(slice: Image) -> bytes:
+    width, _ = slice.size
+
+    rows_bytes = []
+    for row in range(3):
+        box = (0, row*8, width, (row+1)*8)
+        row_bytes = slice.crop(box).transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT).tobytes()
+        rows_bytes.append(row_bytes)
+
+    all_bytes = bytes()
+    for i in range(width):
+        all_bytes += (rows_bytes[0][i]).to_bytes(1, byteorder="little")
+        all_bytes += (rows_bytes[1][i]).to_bytes(1, byteorder="little")
+        all_bytes += (rows_bytes[2][i]).to_bytes(1, byteorder="little")
+
+    return all_bytes
+
+@click.command()
+@click.option("-w", "--width", default=384)
+@click.argument("input_file")
+@click.argument("output_file")
+def generate_cpp_code(width, input_file, output_file):
+    with Image.open(input_file) as im:
+        resized_im = ImageOps.contain(im, (width, 10000))
+        bw_im = resized_im.convert(mode="1", dither=FLOYDSTEINBERG)
+
+        # invert pixels
+        l_im = bw_im.convert('L')
+        inv_l_im = ImageOps.invert(l_im)
+        inverted_im = inv_l_im.convert('1')
+
+        with open(output_file, "wb") as out:
+            for slice in get_slices(inverted_im):
+                data = slice_to_bytes(slice)
+                out.write(data)
+
+
+        # create CPP variable declaration
+        # data = inverted_im.tobytes()
+
+    # with open(output_file, "wb") as out:
+    #     out.write(data)
+
+
+if __name__ == "__main__":
+    generate_cpp_code()
