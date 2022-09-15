94284b9c02839b99b0c97547e498877d8cfa3cdd
Quentin Suire <dev@quentinsuire.com>
2022-09-15 14:45:18 +0200

    add button and led

--- electronic/big-button-printer/src/main.cpp
+++ electronic/big-button-printer/src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+#include <AceButton.h>
+#include <jled.h>
 #include <LittleFS.h>
 
 #include "TPrinter.h"
 
 
+// Printer
 const byte dtrPin = D1;  // GPIO 5
 const byte rxPin = D9;  // GPIO 3
 const byte txPin = D10;  // GPIO 1
@@ -16,6 +19,13 @@ const int printerBaudrate = 9600;
 SoftwareSerial mySerial(rxPin, txPin);
 Tprinter printer(&mySerial, printerBaudrate);
 
+// Button
+const byte led_pin = D5;
+const byte button_pin = D2;
+JLed led = JLed(led_pin);
+ace_button::AceButton button(button_pin, LOW);
+
+
 void setupPrinter() {
     mySerial.begin(printerBaudrate);
     printer.begin();
@@ -24,15 +34,48 @@ void setupPrinter() {
     printer.setCharset(CHARSET_USA);
 }
 
-void setup() {
-    setupPrinter();
+void onButtonClicked() {
+    printer.println("Click");
+    // File image_file = LittleFS.open("/384.raw", "r");
+    // printer.printRawImage(&image_file, 384);
+}
 
+void onButtonDoubleClicked() {
+    printer.println("Double click");
+}
+
+void handleEvent(ace_button::AceButton * /* button */, uint8_t eventType, uint8_t /* buttonState */) {
+    switch (eventType) {
+        case ace_button::AceButton::kEventClicked:
+        case ace_button::AceButton::kEventReleased:
+            onButtonClicked();
+            break;
+        case ace_button::AceButton::kEventDoubleClicked:
+            onButtonDoubleClicked();
+            break;
+    }
+}
+
+void setup() {
     LittleFS.begin();
 
-    File image_file = LittleFS.open("/quentin.raw", "r");
-    printer.printRawImage(&image_file, 384);
+    setupPrinter();
+
+    // Button & LED
+    pinMode(led_pin, OUTPUT);
+    pinMode(button_pin, INPUT_PULLUP);
+
+    ace_button::ButtonConfig *buttonConfig = button.getButtonConfig();
+    buttonConfig->setEventHandler(handleEvent);
+    buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureDoubleClick);
+    buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureSuppressClickBeforeDoubleClick);
+    buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureSuppressAfterClick);
+    buttonConfig->setFeature(ace_button::ButtonConfig::kFeatureSuppressAfterDoubleClick);
+
+    led.Breathe(2000).Forever();
 }
 
 void loop() {
-    delay(100);
+    button.check();
+    led.Update();
 }
